46999e264d9817d140013889aa0084647f4252f8
samuel <samuelgiraldoroldan51@gmail.com>
2025-05-19 19:39:50 -0500

    preubas automatizadas cypress y selenium

--- selenium_tests/utils/driverSetup.js
+++
@@ -1,9 +0,0 @@
-const { Builder } = require('selenium-webdriver');
-
-async function getDriver() {
-    // Configura el WebDriver para Chrome
-    const driver = await new Builder().forBrowser('chrome').build();
-    return driver;
-}
-
-module.exports = { getDriver };
